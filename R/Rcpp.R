b561cb20f907745be32b9b545cbe7b77e2ad4ac9
Marek Kurdej <marek@quasardb.net>
2018-06-07 17:57:56 +0200

    Split column_type.R.

--- R/Rcpp.R
+++ R/Rcpp.R
@@ -3,21 +3,3 @@
 #' @exportPattern "^[[:alpha:]]+"
 #' @importFrom Rcpp sourceCpp
 NULL
-
-#' @title Type of a timeseries column.
-#'
-#' Possible types of a timeseries column.
-#'
-#' @description Currently, a timeseries can have columns of the following types:
-#'   - double    - column of floating point values.
-#'   - blob      - column of binary data.
-#'   - integer   - column of signed 64-bit integer values.
-#'   - timestamp - column of nanosecond-precision timestamps.
-#'
-#' @export
-column_type <- list(
-    double = 0L,
-    blob = 1L,
-    integer = 2L,
-    timestamp = 3L
-)
