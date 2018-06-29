3c4db34dd2c63b24bdfab0a888f5536f3127dd1f
Marek Kurdej <marek@quasardb.net>
2018-06-29 10:47:49 +0200

    Run qdbd database automatically when knitting readme.

--- tests/testthat/setup_qdbd.R
+++ tests/testthat/setup_qdbd.R
@@ -1,6 +1,6 @@
 message("**** Running qdbd daemon... *****")
 
-qdbd <- run_qdbd(root = file.path(getwd(), "..", ".."))
+qdbd <- run_database(root = file.path(getwd(), "..", ".."))
 
 expect(qdbd$process$is_alive(),
        failure_message = "qdbd process should still be alive")
