5f6e59c4c47134adee8217ddebcd9b6700b215d0
Marek Kurdej <marek@quasardb.net>
2020-02-04 14:27:11 +0100

    [tests] Remove tautological assertion.

--- tests/testthat/test_connect.R
+++ tests/testthat/test_connect.R
@@ -1,19 +1,16 @@
 context("connect")
 
 test_that("returns invalid argument when URI is invalid", {
-  expect_error(handle <- connect("invalid URI"),
-    regexp = "The argument is invalid"
-  )
+  expect_error(connect("invalid URI"),
+               regexp = "The argument is invalid")
 })
 
 test_that("returns invalid argument when cluster is down", {
-  expect_error(handle <- connect("qdb://127.0.0.1:1"),
-    regexp = "Connection refused"
-  )
+  expect_error(connect("qdb://127.0.0.1:1"),
+               regexp = "Connection refused")
 })
 
 test_that("connects successfully to the existing cluster using default URI", {
-  expect_equal("qdb://127.0.0.1:2836", "qdb://127.0.0.1:2836")
   handle <- connect()
   succeed(message = "successfully connected to quasardb cluster")
 })
